0d897269746794fa3b2b6177fefbade0ea5e4533
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-22 11:18:54 -0300

    FGTS finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal porcentual = 8;
+            decimal fundoG;
+            decimal valor;
+
+            Console.WriteLine("---Cálculo de FGTS---");
+            Console.Write("Digite o valor..: ");
+            valor = Convert.ToDecimal(Console.ReadLine());
+            fundoG = (valor/100) * porcentual;
+            Console.Write($"Com base no seu salário de {valor:C}, seu FGTS é..: {fundoG:C2}");
+
+        }
+    }
+}
